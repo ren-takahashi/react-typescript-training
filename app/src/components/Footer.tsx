d35f81b4e365b64bdee89657c653b4e5b53886b4
takahashiren <roroy1009@gmail.com>
2026-03-01 13:05:21 +0900

    todoアプリの開発に不要なお試し実装のファイルを削除

--- app/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-// 【React】ヘッダーコンポーネント
-// 【JavaScript】export default で外部から import 可能にする
-export default function Header() {
-  return (
-    <header style={{ backgroundColor: "#333", color: "#fff", padding: "16px" }}>
-      <h1>My Learning App</h1>
-    </header>
-  );
-}
